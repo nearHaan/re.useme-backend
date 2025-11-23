06dcc9509f13ca71216a64e3840c7dacbc86a6ac
nearHaan <farhaan.nizam675@gmail.com>
2025-11-23 19:33:27 +0530

    connect mongodb atlas

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,6 +8,15 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
+
+mongoose
+  .connect("")
+  .then(() => {
+    console.log("Connected to database successfully");
+  })
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
