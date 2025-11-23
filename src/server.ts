8d9b057aed8192f5fec632d1d28247b7204a95c1
nearHaan <farhaan.nizam675@gmail.com>
2025-11-23 19:40:01 +0530

    add dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,14 +13,13 @@ app.get("/", (req, res) => {
 });
 
 mongoose
-  .connect("")
+  .connect(process.env.DB_URL as string)
   .then(() => {
     console.log("Connected to database successfully");
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
   })
   .catch((err) => {
     console.log("Error: ", err);
   });
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
