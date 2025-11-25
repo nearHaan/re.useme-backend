8863df19488b6ab85f7059a07a5590c2bc4702b7
nearHaan <farhaan.nizam675@gmail.com>
2025-11-26 00:11:38 +0530

    add routes

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import UserData from "./models/userDataModel.js";
-import User from "./models/userModel.js";
+import indexRoutes from "./routes/index.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Welcome");
-});
-
+app.use("/api", indexRoutes);
 mongoose
   .connect(process.env.DB_URL as string)
   .then(async () => {
