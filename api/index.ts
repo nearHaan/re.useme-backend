69d831eeaf522700ae9098348a9ab2aa5ec0a547
nearHaan <farhaan.nizam675@gmail.com>
2025-11-26 19:28:04 +0530

    attempt to fix vercel

--- api/index.ts
+++
@@ -1,9 +0,0 @@
-import app from "../src/server.js";
-import serverless from "serverless-http";
-
-export const GET = serverless(app);
-export const POST = serverless(app);
-export const PUT = serverless(app);
-export const PATCH = serverless(app);
-export const DELETE = serverless(app);
-export const OPTIONS = serverless(app);
