f25b83addf7058c3accd371967e0986f64494272
nearHaan <farhaan.nizam675@gmail.com>
2025-11-26 22:01:02 +0530

    port updated

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 async () => {
   await connectDB();
 
-  app.listen(3000, () => {
+  app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
   });
 };
