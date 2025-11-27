e5ef78d15b78ee7cb3d2fd53f2b1354da5bab3cf
nearHaan <farhaan.nizam675@gmail.com>
2025-11-28 01:00:55 +0530

    delete sample userDetails

--- src/controllers/data.ts
+++ src/controllers/data.ts
@@ -55,17 +55,7 @@ export const getDataTemplate = async (req: Request, res: Response) => {
     }
     res.status(200).json({
       template: dataObj,
-      userDetails: {
-        "692740f422209091d5df45be": {
-          "6927513e731ac7ff646b3ce0":
-            "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww",
-          "692750c0731ac7ff646b3cd5": "Farhaan",
-          "692750ce731ac7ff646b3cd8": "Nizam",
-          "69275125731ac7ff646b3cdd": "buhahahahah",
-        },
-        "6927415d22209091d5df45c3": [],
-        "6927417d22209091d5df45c5": [],
-      },
+      userDetails: {},
     });
   } catch (err: any) {
     res.status(500).json({ error: err });
